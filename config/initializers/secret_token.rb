24c0f8cdeae0e01d4ee17fae4208b5fff79fa20d
larry berland <larryberland@gmail.com>
2012-01-08 13:19:57 -0500

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KimbraStudio::Application.config.secret_token = 'ee7046f8156fd671f2a54f428dd06996995f53cd1b20df1c09e91bb8bdbe656571495ebb3a0e49f347139ee1bc05409848e766063d50c8c31a101a64de7cbdbc'
