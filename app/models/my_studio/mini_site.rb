515571b49caa9a20ee9bbd329be2dad084505102
Jim James <jim@jimjames.org>
2012-03-02 19:45:29 +1000

    make minisite attrs accessible. how did I not do that before?!?

--- app/models/my_studio/mini_site.rb
+++ app/models/my_studio/mini_site.rb
@@ -1,5 +1,9 @@
 class MyStudio::MiniSite < ActiveRecord::Base
-  attr_accessible :image, :remote_image_url
+
+  attr_accessible :image, :remote_image_url, :bgcolor, :font_color, :font_family
+
   belongs_to :studio
+
   mount_uploader :image, ImageUploader
+
 end
