9c868aec1e861d028d1481b86411ad68c8293973
larry berland <larryberland@gmail.com>
2012-03-29 07:17:14 -0400

    Set a price if one doesn't exist

--- app/models/shopping/item.rb
+++ app/models/shopping/item.rb
@@ -4,8 +4,31 @@ class Shopping::Item < ActiveRecord::Base
   belongs_to :offer, :class_name => 'Admin::Customer::Offer'
 
   attr_accessible :offer, :offer_attributes, :offer_id,
-                 :cart,   :cart_attributes, :cart_id
+                  :cart, :cart_attributes, :cart_id
 
   accepts_nested_attributes_for :offer, :cart
 
+  def price
+    v =if offer
+         if offer.piece
+           if offer.piece.price
+             offer.piece.price
+           else
+             "KBS::Missing price for piece=>#{offer.piece.inspect}"
+           end
+         else
+           "KBS::Missing piece info for offer=>#{offer.inspect}"
+         end
+       else
+         "KBS::Missing offer for item=>#{item.inspect}"
+       end
+    if v.kind_of? String
+      # tell jim we don't have price data
+      Rails.logger.warn(v)
+      v = 200.0
+    elsif v < 1.0
+      v = 200.0
+    end
+    v
+  end
 end
