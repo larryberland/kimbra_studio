72ac77626fee4ec17181a0fbdbcb4733afea9a94
Jim James <jim@jimjames.org>
2012-04-27 19:04:19 -0400

    add controller.

---
+++ app/controllers/minisite/stories_controller.rb
@@ -0,0 +1,31 @@
+class StoriesController < ApplicationController
+
+  skip_filter :setup_story
+
+  def index
+    @dates = ('2012-04-14'.to_date..Date.today)
+    @with_names = Story.with_name.group("date(created_at)").size
+    @without_names = Story.without_name.group("date(created_at)").size
+    @stories = Story.today
+    @heading = "Stories for today"
+    render :layout => 'brief'
+  end
+
+  def fetch
+    date = params[:date].to_date rescue Date.today
+    type = params[:type]
+    ip = params[:ip_address]
+    case type
+      when 'with'
+        @stories = Story.with_name.on_date(date)
+        @heading = "Named Stories for #{date.to_s(:db)}"
+      when 'without'
+        @stories = Story.without_name.on_date(date)
+        @heading = "Anonymous Stories for #{date.to_s(:db)}"
+      when 'related'
+        @stories = Story.where(:ip_address => ip).order("date DESC")
+        @heading = "Stories from #{ip}"
+    end
+  end
+
+end
