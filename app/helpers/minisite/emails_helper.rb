328041c6f26da0eae220211cd904d6a629417951
jimjames99 <jim@jimjames.org>
2012-06-11 20:55:51 -0400

    barely passable order status page.

---
+++ app/helpers/minisite/emails_helper.rb
@@ -0,0 +1,11 @@
+module Minisite::EmailsHelper
+
+  def status_for_cart(cart)
+     if cart.shipping.tracking
+       link_to "Click here to check shipment status", "http://http://fuhry.us/packagestalker/track/ups/#{cart.shipping.tracking}"
+     else
+       'Your order has not been shipped yet. We\'ll send a status email when it does.'
+     end
+   end
+
+end
