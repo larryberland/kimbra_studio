ee45f923254ab8dde178e86ccd029a42a18139f1
jimjames99 <jim@jimjames.org>
2012-06-15 07:59:23 -0400

    Add pages for Kimbra to add the UPS delivery tracking number to an order. Learned almost as much as Larry about routes in the process.

--- app/controllers/shopping/carts_controller.rb
+++ app/controllers/shopping/carts_controller.rb
@@ -1,7 +1,51 @@
 module Shopping
-
   class CartsController < BaseController
 
-  end
+    skip_before_filter :authenticate_user!,
+                       only: [:find_by_tracking, :edit_delivery_tracking, :update_delivery_tracking]
+    skip_before_filter :set_client_and_cart, :setup_story,
+                       only: [:find_by_tracking]
 
+    def find_by_tracking
+      @cart = Shopping::Cart.new
+      render layout: false
+    end
+
+    def edit_delivery_tracking
+      if params[:shopping_cart] && params[:shopping_cart][:tracking].present?
+        if @cart = Shopping::Cart.find_by_tracking(params[:shopping_cart][:tracking])
+          @shipping = @cart.shipping
+          # allow to render.
+        else
+          flash[:notice] = "Could not find any order with number like #{params[:shopping_cart][:tracking]}."
+          return redirect_to '/delivery'
+        end
+      else
+        flash[:notice] = 'Need an order number. Like k2s4f921ms '
+        return redirect_to '/delivery'
+      end
+    end
+
+    def update_delivery_tracking
+      if @cart = Shopping::Cart.find_by_tracking(params[:id])
+        if tracking = params[:shopping_shipping] && params[:shopping_shipping][:tracking]
+          @shipping = @cart.shipping
+          @shipping.tracking = tracking
+          if @shipping.save
+            flash[:notice] = "Delivery tracking number #{@shipping.tracking} saved for #{@cart.address.last_name}."
+            return redirect_to '/delivery'
+          else # errors on save
+            return render :edit_delivery_tracking
+          end
+        else # no tracking
+          flash[:error] = 'Must enter a tracking number.'
+          return render :edit_delivery_tracking
+        end
+      else # no cart
+        flash[:error] = "Could not find order with number #{params[:id]}"
+        return redirect_to '/delivery'
+      end
+    end
+
+  end
 end
--- app/models/shopping/shipping.rb
+++ app/models/shopping/shipping.rb
@@ -13,10 +13,42 @@ class Shopping::Shipping < ActiveRecord::Base
 
   accepts_nested_attributes_for :cart
 
+  # 1Z xxx xxx yy zzzz zzz c
+  # xxx xxx is the alphanumeric account number of the shipper, Kimbra's: A51 47X
+  # yy is the service code
+  # UPS Standard 	11
+  # UPS Ground 	03
+  # UPS 3 Day Select 	12
+  # UPS 2nd Day Air 	02
+  # UPS 2nd Day Air AM 	59
+  # UPS Next Day Air Saver 	13
+  # UPS Next Day Air 	01
+  # 01	UPS United States Next Day Air ("Red")
+  # 02	UPS United States Second Day Air ("Blue")
+  # 03	UPS United States Ground
+  # 12	UPS United States Third Day Select
+  # 13	UPS United States Next Day Air Saver ("Red Saver")
+  # 15	UPS United States Next Day Air Early A.M.
+  # 22	UPS United States Ground - Returns Plus - Three Pickup Attempts
+  # 32	UPS United States Next Day Air Early A.M. - COD
+  # 33	UPS United States Next Day Air Early A.M. - Saturday Delivery, COD
+  # 41	UPS United States Next Day Air Early A.M. - Saturday Delivery
+  # 42	UPS United States Ground - Signature Required
+  # 44	UPS United States Next Day Air - Saturday Delivery
+  # 66	UPS United States Worldwide Express
+  # zzzz zzz is the package identifier
+  # c is a checksum
+  @tracking_regex = /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/
+
   validates_presence_of :cart
   validates_presence_of :shipping_option
   validates_presence_of :total_cents
+  validates :tracking, format: {with: @tracking_regex, message: 'UPS tracking numbers look like 1Z xxx xxx yy zzzz zzz c'}
 
-  private #=======================================================
+  def tracking=(trk)
+    trk.upcase!
+    trk.gsub!(/[^\w^\d\s]/,'')
+    write_attribute :tracking, trk
+  end
 
 end
