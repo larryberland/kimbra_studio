f8673d3e87082f0b5434387a480048f76cb0b8b8
jimjames99 <jim@jimjames.org>
2012-06-24 07:57:22 -0400

    change fog initializer to read from KIMBRA_STUDIO_CONFIG

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,12 +1,12 @@
-info = YAML.load_file(Rails.root.join('config','amazon_s3.yml'))[Rails.env]
+# info = YAML.load_file(Rails.root.join('config','amazon_s3.yml'))[Rails.env]
 
 CarrierWave.configure do |config|
    config.fog_credentials = {
      :provider               => 'AWS',
-     :aws_access_key_id      => info['access_key_id'],
-     :aws_secret_access_key  => info['secret_access_key'],
+     :aws_access_key_id      => KIMBRA_STUDIO_CONFIG[:s3]['access_key_id'],
+     :aws_secret_access_key  => KIMBRA_STUDIO_CONFIG[:s3]['secret_access_key'],
    }
-   config.fog_directory  = info['bucket_name']
+   config.fog_directory  = KIMBRA_STUDIO_CONFIG[:s3]['bucket_name']
    config.fog_public     = false
    config.fog_attributes = {}
    #config.fog_attributes = {'Cache-Control' => 'max-age=315576000'} # for static assets that can be cached by the browser for a long time.
