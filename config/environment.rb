886886d54f7e0753d477e4580eddd27b768c814f
larry berland <larryberland@gmail.com>
2012-06-24 08:20:51 -0400

    Asset pipeline, not sure why database.yml is in here when we have a .gitignore.

--- config/environment.rb
+++ config/environment.rb
@@ -8,24 +8,22 @@ begin
   # Base config values common to all environments.
   KIMBRA_STUDIO_CONFIG = YAML.load_file('config/kimbra_studio.yml')[Rails.env]
 
+  # Uses local sensitive.yml file - get a copy from Larry or Jim.
+  sensitive            = YAML.load_file('config/sensitive.yml')[Rails.env]
+  sensitive.each do |k, v|
+    KIMBRA_STUDIO_CONFIG[k].merge!(v)
+  end
+
   if Rails.env.production?
     # Load these from heroku ENV vars. Run xxx ruby script to load heroku with any updates to sensitive.yml.
     KIMBRA_STUDIO_CONFIG.each do |top_level_key, top_level_value_hash|
       # Skip keys whose values are not hashes. We want things like :s3=>{:username=>'abc',:pw=>'123'}
-      if top_level_value_hash.is_a? Hash
-        top_level_value_hash.each do |k, v|
-          heroku_env_key = "#{top_level_key}_#{k}"
-          if ENV[heroku_env_key]
-            KIMBRA_STUDIO_CONFIG[top_level_key][k] = v
-          end
+      top_level_value_hash.each do |k, v|
+        heroku_env_key = "#{top_level_key}_#{k}"
+        if ENV[heroku_env_key]
+          KIMBRA_STUDIO_CONFIG[top_level_key][k] = v
         end
-      end
-    end
-  else
-    # Uses local sensitive.yml file - get a copy from Larry or Jim.
-    sensitive = YAML.load_file('config/sensitive.yml')[Rails.env]
-    sensitive.each do |k, v|
-      KIMBRA_STUDIO_CONFIG[k].merge!(v)
+      end if top_level_value_hash.is_a? Hash
     end
   end
 rescue Exception => e
