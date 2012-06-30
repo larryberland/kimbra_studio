7d60046b93f20ec7c0559eeedbc2d8d283149da4
jimjames99 <jim@jimjames.org>
2012-06-30 18:00:51 -0400

    sort out Stripe api keys for heroku.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,2 @@
-KIMBRA_STUDIO_CONFIG[:stripe][:test_key] ||= ENV['STRIPE_KEY'] # test key
-KIMBRA_STUDIO_CONFIG[:stripe][:test_key] ||= KIMBRA_STUDIO_CONFIG[:stripe][:secret_key] # test key
-
-# going to use the test_key for now
-Stripe.api_key    = KIMBRA_STUDIO_CONFIG[:stripe][:test_key]
-STRIPE_PUBLIC_KEY = KIMBRA_STUDIO_CONFIG[:stripe][:publishable_key_test]
+Stripe.api_key    = KIMBRA_STUDIO_CONFIG[:stripe][:secret_key]
+STRIPE_PUBLIC_KEY = KIMBRA_STUDIO_CONFIG[:stripe][:publishable_key]
