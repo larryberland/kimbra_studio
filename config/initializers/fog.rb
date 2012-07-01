eb37a26737ba19006ad6381c0a40224b6cc09c42
jimjames99 <jim@jimjames.org>
2012-07-01 09:45:55 -0400

    adding fog_public = true back in.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
      :aws_secret_access_key  => KIMBRA_STUDIO_CONFIG[:s3]['secret_access_key'],
    }
    config.fog_directory  = KIMBRA_STUDIO_CONFIG[:s3]['bucket_name']
-   config.fog_public     = false
+   config.fog_public     = true
    config.fog_attributes = {}
    time_out = (5 * 24) * 60 * 60 # 5 days worth of seconds
    config.fog_attributes = {'Cache-Control' => "max-age=#{time_out}"} # for static assets that can be cached by the browser for a long time.
