05fab675836a80bbac2471191930893b3637b0ba
jimjames99 <jim@jimjames.org>
2012-07-02 07:11:05 -0400

    added consumer phone number to shopping_addresses -- need to db:reset, sorry!

--- db/migrate/20120317204651_create_shopping_addresses.rb
+++ db/migrate/20120317204651_create_shopping_addresses.rb
@@ -11,9 +11,11 @@ class CreateShoppingAddresses < ActiveRecord::Migration
       t.string :zip_code
       t.string :country, default: 'USA'
       t.string :email
+      t.string :phone
 
       t.timestamps
     end
     add_index :shopping_addresses, :state_id
+    add_index :shopping_addresses, :phone
   end
 end
