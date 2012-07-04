0b69dc2e814f5a5d4160258587650d8fbc10ab77
jimjames99 <jim@jimjames.org>
2012-07-04 17:41:08 -0400

    Update descriptions of all pieces - eliminate necklace options and rings (they come in sizes). Change fog expiry to 15 days. Add extra descriptions to offer/_show.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
    config.fog_directory  = KIMBRA_STUDIO_CONFIG[:s3]['bucket_name']
    config.fog_public     = true
    config.fog_attributes = {}
-   time_out = (5 * 24) * 60 * 60 # 5 days worth of seconds
+   time_out = (15 * 24) * 60 * 60 # 15 days worth of seconds
    config.fog_attributes = {'Cache-Control' => "max-age=#{time_out}"} # for static assets that can be cached by the browser for a long time.
    config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
