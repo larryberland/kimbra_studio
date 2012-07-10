cd951e4ab775f7eb7772d717678d1bf56d63bb09
jimjames99 <jim@jimjames.org>
2012-07-09 21:38:01 -0400

    put US states before Canadian provinces.

--- app/models/state.rb
+++ app/models/state.rb
@@ -32,7 +32,7 @@ class State < ActiveRecord::Base
   # @param [none]
   # @return [ Array[Array] ]
   def self.form_selector
-    find(:all, :order => 'country_id ASC, abbreviation ASC').collect { |state| [state.abbrev_and_name, state.id] }
+    find(:all, :order => 'country_id DESC, abbreviation ASC').collect { |state| [state.abbrev_and_name, state.id] }
   end
 
   # filter all the states for a form for a given country_id
