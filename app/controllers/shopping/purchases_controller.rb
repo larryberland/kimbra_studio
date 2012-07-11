10079bdcdfd465b605a20dc1811aebc2c9ae726e
jimjames99 <jim@jimjames.org>
2012-07-11 07:26:22 -0400

    clean up inconsistent references to cart. make addresses a bit neater.

--- app/controllers/shopping/purchases_controller.rb
+++ app/controllers/shopping/purchases_controller.rb
@@ -17,7 +17,6 @@ module Shopping
         if @purchase.errors.present?
           edit_shopping_cart_purchase_path(@cart)
         else
-          # TODO Convert this to sending mailer in background.
           ClientMailer.delay.send_order_confirmation(@cart.id, @studio.id)
           # After the credit card is run (successful create) we need to close out
           # this cart so that the consumer will start a new one if they want to make
