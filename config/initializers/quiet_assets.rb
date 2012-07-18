926916dce50a3d559e18bedb97259bb1a9b174bb
jimjames99 <jim@jimjames.org>
2012-07-18 18:31:32 -0400

    quieten the logs by suppressing the assets items.

--- config/initializers/quiet_assets.rb
+++ config/initializers/quiet_assets.rb
@@ -1,22 +1,13 @@
-#Rails.application.assets.logger = Logger.new('/dev/null')
-#
-## Prevents assets pipeline items from showing up in the logs by
-## forcing them to only be logged at the error level (not info level, etc).
-#
-## When rails 3.2 is done, we can use this:
-## config.assets.logger = nil
-## See rails ticket: https://github.com/rails/rails/issues/2639  etc...
-#
-#Rails::Rack::Logger.class_eval do
-#
-#  def call_with_quiet_assets(env)
-#    previous_level = Rails.logger.level
-#    Rails.logger.level = Logger::ERROR if env['PATH_INFO'].index("/assets/") == 0
-#    call_without_quiet_assets(env).tap do
-#      Rails.logger.level = previous_level
-#    end
-#  end
-#
-#  alias_method_chain :call, :quiet_assets
-#
-#end
+# Prevents assets pipeline items from showing up in the logs by
+# forcing them to only be logged at the error level (not info level, etc).
+
+Rails::Rack::Logger.class_eval do
+  def call_with_quiet_assets(env)
+    previous_level = Rails.logger.level
+    Rails.logger.level = Logger::ERROR if env['PATH_INFO'].index("/assets/") == 0
+    call_without_quiet_assets(env).tap do
+      Rails.logger.level = previous_level
+    end
+  end
+  alias_method_chain :call, :quiet_assets
+end
