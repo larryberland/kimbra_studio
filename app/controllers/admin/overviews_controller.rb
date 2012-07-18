fe644f82f75dd429f01a86ca176366eb84c7d58b
jimjames99 <jim@jimjames.org>
2012-07-18 18:36:04 -0400

    add Session view for admin so they can generate email offers from a session.

--- app/controllers/admin/overviews_controller.rb
+++ app/controllers/admin/overviews_controller.rb
@@ -3,6 +3,7 @@ class Admin::OverviewsController < ApplicationController
   # GET /admin/overview
   # GET /admin/overview.json
   def show
+    @sessions = MyStudio::Session.all
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @admin_overview }
