833bb193ee3c354d5fb3a85db1f8bb453f8aab3f
larry berland <larryberland@gmail.com>
2012-07-21 13:43:29 -0400

    increment assests version

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module KimbraStudio
     end
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version                  = '1.0'
+    config.assets.version                  = '1.1'
 
     # Rspec generator functionality
     # :fixtures => true specifies to generate a fixture for each model
