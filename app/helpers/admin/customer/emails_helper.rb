fb982565835df5e550a1aad76c8585cfd128310a
jimjames99 <jim@jimjames.org>
2012-08-12 18:40:09 -0400

    I am loving being able to look into the queue.

--- app/helpers/admin/customer/emails_helper.rb
+++ app/helpers/admin/customer/emails_helper.rb
@@ -1,3 +1,13 @@
 module Admin::Customer::EmailsHelper
 
+  def sent_or_queue(admin_customer_email)
+    if admin_customer_email.in_send_offers_queue?
+      'in queue'
+    elsif admin_customer_email.sent_at?
+      time_short(admin_customer_email.sent_at)
+    else
+      'not sent'
+    end
+  end
+
 end
