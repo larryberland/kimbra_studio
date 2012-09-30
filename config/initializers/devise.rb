e6f0af08226a7a525e9f60801514a50fdd948125
jimjames99 <jim@jimjames.org>
2012-09-30 14:03:45 -0400

    fix bad devise config.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -77,7 +77,7 @@ Devise.setup do |config|
   # able to access the website for two days without confirming his account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming his account.
-  config.confirm_within = 2.weeks
+  # config.confirm_within = 2.weeks
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [ :email ]
@@ -87,7 +87,7 @@ Devise.setup do |config|
    config.remember_for = 2.weeks
 
   # If true, a valid remember token can be re-used between multiple browsers.
-  config.remember_across_browsers = true
+  # config.remember_across_browsers = true
 
   # If true, extends the user's remember period when remembered via cookie.
    config.extend_remember_period = true
