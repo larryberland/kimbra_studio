4850046ec43c42a3779e936d9dd7c8e395d449b2
jimjames99 <jim@jimjames.org>
2012-10-02 22:13:51 -0400

    update wording for email. move around links on Studio#index.

--- app/helpers/studios_helper.rb
+++ app/helpers/studios_helper.rb
@@ -1,14 +1,22 @@
 module StudiosHelper
 
-  def email_or_link(studio)
+  def eap_link(studio)
     if studio.owner
       if SentEmail.sent_studio_eap_email?(studio.owner.email)
-        studio.owner.email
+        ''
       else
         content_tag :span, {id: "send_new_account_email_#{studio.id}"} do
           link_to 'send Early Adopter email', send_new_account_email_studio_path(studio), method: :post, remote: true
         end
       end
+    else
+
+    end
+  end
+
+  def owner_link(studio)
+    if studio.owner
+      mail_to studio.owner.email, studio.owner.name
     else
       link_to 'create', new_owner_studio_path(studio)
     end
