e5dc6c27bc665d51ebce134320d2e3bce2cf97e8
larry berland <larryberland@gmail.com>
2012-10-06 09:21:58 -0400

    Offer Emails Index new format

--- app/helpers/admin/customer/emails_helper.rb
+++ app/helpers/admin/customer/emails_helper.rb
@@ -4,7 +4,7 @@ module Admin::Customer::EmailsHelper
     if admin_customer_email.in_send_offers_queue?
       'in queue'
     elsif admin_customer_email.sent_at?
-      time_short(admin_customer_email.sent_at)
+      time_short_index(admin_customer_email.sent_at)
     else
       'not sent'
     end
