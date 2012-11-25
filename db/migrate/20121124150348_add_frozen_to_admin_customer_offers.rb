5186e98773bb59e2227cc704cef3fbc6af5ce4eb
jimjames99 <jim@jimjames.org>
2012-11-25 15:58:23 -0500

    adding new piece PNGs. Run rake kimbra:seed_kkp do not run seed_rings yet - still working on that. Run rake db:migrate !!

--- db/migrate/20121124150348_add_frozen_to_admin_customer_offers.rb
+++ db/migrate/20121124150348_add_frozen_to_admin_customer_offers.rb
@@ -1,5 +1,7 @@
 class AddFrozenToAdminCustomerOffers < ActiveRecord::Migration
+
   def change
-    add_column :admin_customer_offers, :frozen, :boolean, default: false
+    add_column :admin_customer_offers, :frozen_offer, :boolean, default: false
   end
+
 end
