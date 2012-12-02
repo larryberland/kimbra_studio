0490f6e6c59dd71d0857b1a5f873be02fdcadf76
larry berland <larryberland@gmail.com>
2012-12-02 14:26:38 -0500

    Remove some holiday from year option

--- app/helpers/shopping/items_helper.rb
+++ app/helpers/shopping/items_helper.rb
@@ -3,13 +3,18 @@ module Shopping::ItemsHelper
   def options_for_shopping_item(item)
     option  = nil
     options = if (item.offer.piece)
+                name = item.offer.piece.name
                 case item.offer.piece.category
                   when 'Photo Rings'
                     option = :ring_size
                     options_for_select(%w(6 7 8 9 10 11 12 13), item.option_selected)
                   when 'Holiday'
-                    option = :year
-                    options_for_select(%w(2007 2008 2009 2010 2011 2012), item.option_selected)
+                    if (name == 'Evergreen Ornament' || name == 'Memorial Ornament' || name == 'Aspen Ornament')
+                      nil
+                    else
+                      option = :year
+                      options_for_select(%w(2007 2008 2009 2010 2011 2012), item.option_selected)
+                    end
                   else
                     nil
                 end
