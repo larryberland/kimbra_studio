5d44f5b2d8f20b4d9d5bded976bfee54c3160bd1
larry berland <larryberland@gmail.com>
2012-12-11 06:33:27 -0500

    Pass 5 NavBar is color happy

--- app/models/my_studio/minisite.rb
+++ app/models/my_studio/minisite.rb
@@ -11,10 +11,10 @@ class MyStudio::Minisite < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   validates :bgcolor, :presence => true,
-            :format => {:with => CustomValidators::Colors.css_color_validator}
+            :format             => {:with => CustomValidators::Colors.css_color_validator}
 
   validates :font_color, :presence => true,
-              :format => {:with => CustomValidators::Colors.css_color_validator}
+            :format                => {:with => CustomValidators::Colors.css_color_validator}
 
   FONTS = %w(Georgia Times Palatino Arial Cursive Helvetica Verdana Courier Monaco)
 
@@ -24,13 +24,36 @@ class MyStudio::Minisite < ActiveRecord::Base
     image_height.to_i + 40
   end
 
+  # http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
+  def calc_border_color
+    if (@border_color.nil?)
+      if bgcolor.starts_with?('#')
+        hex = bgcolor[1..-1]
+        if (hex.size > 3)
+          rgb = hex.scan(/../).map { |color| color.to_i(16) }
+        else
+          rgb = hex.scan(/../).map { |color| color.to_i(16) }
+        end
+        brightness = Math.sqrt(0.299 * (rgb[0]*rgb[0]) + 0.587 * (rgb[1]*rgb[1]) + 0.114 * (rgb[2]*rgb[2]))
+        if (brightness < 130)
+          # background is dark use white
+          @border_color = "#ffffff"
+        else
+          # use black
+          @border_color = "#000000"
+        end
+      end
+      @border_color
+    end
+  end
+
   private
 
   def check_logo_size
     if (image_width.nil? or image_height.nil?)
       if (image)
         if img = image.to_image
-          self.image_width = img.columns
+          self.image_width  = img.columns
           self.image_height = img.rows
         end
       end
