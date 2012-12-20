4ea94bb32e724534643e0a7b9f7fb56909c55aff
Jim James <jim@jimjames.org>
2012-12-20 15:04:08 -0500

    precompile everything!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ KimbraStudio::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( minisite.js minisite.css stories.css orbit-1.2.3.css )
   config.assets.precompile += %w( bootstrap-ie6.min.css bootstrap.css )
-  config.assets.precompile += %w( platypus_dd.js data_tables/pagination.js data_tables/sort_dates_with_nils.js data_tables/sort_currency.js )
+  config.assets.precompile += %w( platypus_dd.js )
+  config.assets.precompile += Dir.glob(File.join(Rails.root, '/app/assets/javascripts/data_tables',"*.js"))
   config.assets.precompile += %w( fileuploads.css fileuploads.js xdr.js )
 
   # Disable delivery errors, bad email addresses will be ignored
