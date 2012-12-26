30b4ef52cf0b69584f085ef45a58a625aa445f15
larry berland <larryberland@gmail.com>
2012-12-26 11:42:09 -0500

    buttons are there but don't know if they work anymore

---
+++ app/helpers/my_button_helper.rb
@@ -0,0 +1,26 @@
+module MyButtonHelper
+  # override of submit_tag form_tag_helper
+  def submit_icon_tag(icon_class, value = "Save changes", options = {})
+    options = options.stringify_keys
+
+    if disable_with = options.delete("disable_with")
+      options["data-disable-with"] = disable_with
+    end
+
+    if confirm = options.delete("confirm")
+      options["data-confirm"] = confirm
+    end
+
+    # start of platypus override
+    button_class ||= 'btn btn-primary btn-small'
+    options.merge!("type" => "submit", "name" => "commit", "value" => value, "class" => button_class)
+
+    # use a button tag instead of input
+    content_tag(:button, options) do
+      content_tag(:i, "", "class" => icon_class) + value
+    end
+    #
+    #tag :input, html_options.update(options)
+  end
+
+end
