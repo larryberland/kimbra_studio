3fd73c407182f5cf94691dbe2ba5e4bf715c97fa
larry berland <larryberland@gmail.com>
2012-12-26 06:46:52 -0500

    not sure why i just didn't override form.submit helper instead

--- app/helpers/spinner_helper.rb
+++ app/helpers/spinner_helper.rb
@@ -4,8 +4,13 @@ module SpinnerHelper
   #       id: "my_submit_#{unique_id_with_no_underscores}"
   #       class: 'multi-submits'  if more than one on a page
   #<span id='<%= "spinner_#{value}" %>' style='display:none' class='spinner'></span>
-  def spinner(unique_id, options= {})
-    options[:id]    ||= "spinner_#{unique_id}"
+  def spinner_multi(unique_id, options= {})
+    options[:id] ||= "spinner_#{unique_id}"
+    spinner(options)
+  end
+
+  def spinner(options={})
+    options[:id]    ||= "spinner"
     options[:style] ||= 'display:none'
     options[:class] ||= 'spinner'
     content_tag(:span, '', options)
