c37d3ce1293a70bfcf74e7ea5ff9c9166802b6c9
larry berland <larryberland@gmail.com>
2012-12-26 05:01:32 -0500

    trying to corral the spinners but they are out of control

---
+++ app/helpers/spinner_helper.rb
@@ -0,0 +1,13 @@
+module SpinnerHelper
+
+  # NOTE: your button associated with this spinner must have
+  #       id: "my_submit_#{unique_id_with_no_underscores}"
+  #       class: 'multi-submits'  if more than one on a page
+  #<span id='<%= "spinner_#{value}" %>' style='display:none' class='spinner'></span>
+  def spinner(unique_id, options= {})
+    options[:id]    ||= "spinner_#{unique_id}"
+    options[:style] ||= 'display:none'
+    options[:class] ||= 'spinner'
+    content_tag(:span, '', options)
+  end
+end
