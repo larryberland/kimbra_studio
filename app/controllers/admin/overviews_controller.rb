228ef6ae021b48e3d153b89e9fefbbbaf1f8f0d6
Jim James <jim@jimjames.org>
2012-12-29 12:05:16 -0500

    add unsubscribes and prospect lists to Overviews page.

--- app/controllers/admin/overviews_controller.rb
+++ app/controllers/admin/overviews_controller.rb
@@ -8,6 +8,8 @@ class Admin::OverviewsController < ApplicationController
     @sessions = MyStudio::Session.all
     @emails = Admin::Customer::Email.unsent
     @studio_users = User.with_studio_role
+    @unsubscribes = Unsubscribe.all
+    @prospects = Prospect.all
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @admin_overview }
