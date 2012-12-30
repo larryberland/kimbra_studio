9ac1be938595129ec6f46d1b4365e30dae73d8c5
Jim James <jim@jimjames.org>
2012-12-30 07:57:13 -0500

    add more storylines. remove unused parts of minisite/portraits.

--- app/controllers/minisite/portraits_controller.rb
+++ app/controllers/minisite/portraits_controller.rb
@@ -10,6 +10,7 @@ module Minisite
       @my_studio_session   = @admin_customer_email.my_studio_session
       @my_studio_portraits = MyStudio::Portrait.where(my_studio_session_id: @my_studio_session).order('created_at desc')
       @record_count        = @my_studio_portraits.size
+      @storyline.describe 'Viewing Upload Photos page.'
       respond_to do |format|
         format.html # index.html.erb
         format.json { render json: @my_studio_portraits.collect { |p| p.to_jq_upload }.to_json }
@@ -26,6 +27,7 @@ module Minisite
       end
     end
 
+    # NOT USED IN THIS CONTROLLER.
     def new
       @my_studio_portrait = MyStudio::Portrait.new(:my_studio_session => @my_studio_session)
       respond_to do |format|
@@ -35,6 +37,7 @@ module Minisite
     end
 
     # GET /minisite/emails/:id/portraits/1/edit
+    # NOT USED IN THIS CONTROLLER.
     def edit
       @my_studio_portrait = MyStudio::Portrait.find(params[:id])
     end
@@ -49,6 +52,7 @@ module Minisite
       @my_studio_portrait                   = MyStudio::Portrait.new(p_attrs)
       @my_studio_portrait.my_studio_session = @my_studio_session
       if @my_studio_portrait.save
+        @storyline.describe 'Uploaded new photo.'
         respond_to do |format|
           format.html {
             render :json         => [@my_studio_portrait.to_jq_upload].to_json,
@@ -64,10 +68,10 @@ module Minisite
 
     # PUT /minisite/emails/:id/portraits/1
     # PUT /minisite/emails/:id/portraits/1.json
+    # NOT USED IN THIS CONTROLLER.
     def update
       @my_studio_portrait                   = MyStudio::Portrait.find(params[:id])
       @my_studio_portrait.my_studio_session = @my_studio_session
-
       respond_to do |format|
         if @my_studio_portrait.update_attributes(params[:my_studio_portrait])
           format.html { redirect_to my_studio_session_portraits_url(@my_studio_session), notice: 'Portrait was successfully updated.' }
@@ -81,21 +85,16 @@ module Minisite
 
     # DELETE /minisite/emails/:id/portraits/1
     # DELETE /minisite/emails/:id/portraits/1.json
+    # NOT USED IN THIS CONTROLLER.
     def destroy
       @my_studio_portrait = MyStudio::Portrait.find(params[:id])
       @my_studio_portrait.destroy
       @my_studio_portraits = MyStudio::Portrait.where(my_studio_session_id: @my_studio_session).order('created_at desc')
       @record_count        = @my_studio_portraits.size
-
       respond_to do |format|
         format.html { redirect_to my_studio_session_portraits_url }
         format.json { head :ok }
       end
-    end
-
-    def upload_status_messages
-      @my_studio_portraits = MyStudio::Portrait.where(my_studio_session_id: @my_studio_session).order('created_at desc')
-      @record_count        = @my_studio_portraits.size
     end
 
   end
