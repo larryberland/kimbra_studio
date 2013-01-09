dd9a6e0d40f1b0f2f25de5a659dd0efdaa701f07
Jim James <jim@jimjames.org>
2013-01-09 12:24:54 -0500

    fix for clientSideValidations js for bootstrap.

--- app/assets/javascripts/bootstrap/client-side-validation.js
+++ app/assets/javascripts/bootstrap/client-side-validation.js
@@ -1,30 +1,30 @@
-(function () {
-    $(document).ready(function () {
-        return $("div.control-group").focusout(function () {
-            if (!$("div.control-group").hasClass("info")) {
-                return $(this).addClass("xsuccess");
+(function() {
+    $(document).ready(function() {
+        return $("div.control-group").focusout(function() {
+            if (!$("div.control-group").hasClass("error")) {
+                return $(this).addClass("success");
             }
         });
     });
 }).call(this);
 
-clientSideValidations.formBuilders['ActionView::Helpers::FormBuilder'] = {
-    add:function (element, settings, message) {
+ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] = {
+    add: function(element, settings, message) {
         var errorElement, wrapper;
 
         settings.wrapper_tag = ".control-group";
         settings.error_tag = "span";
         settings.error_class = "help-inline";
-        settings.wrapper_error_class = "info";
-        settings.wrapper_success = "xsuccess";
+        settings.wrapper_error_class = "error";
+        settings.wrapper_success = "success";
 
         if (element.data('valid') !== false) {
             wrapper = element.closest(settings.wrapper_tag);
             wrapper.removeClass(settings.wrapper_success);
             wrapper.addClass(settings.wrapper_error_class);
             errorElement = $("<" + settings.error_tag + "/>", {
-                "class":settings.error_class,
-                text:message
+                "class": settings.error_class,
+                text: message
             });
             return wrapper.find(".controls").append(errorElement);
         } else {
@@ -33,14 +33,14 @@ clientSideValidations.formBuilders['ActionView::Helpers::FormBuilder'] = {
             return element.parent().find("" + settings.error_tag + "." + settings.error_class).text(message);
         }
     },
-    remove:function (element, settings) {
+    remove: function(element, settings) {
         var errorElement, wrapper;
 
         settings.wrapper_tag = ".control-group";
         settings.error_tag = "span";
         settings.error_class = "help-inline";
-        settings.wrapper_error_class = "info";
-        settings.wrapper_success = "xsuccess";
+        settings.wrapper_error_class = "error";
+        settings.wrapper_success = "success";
 
         wrapper = element.closest("" + settings.wrapper_tag + "." + settings.wrapper_error_class);
         wrapper.removeClass(settings.wrapper_error_class);
