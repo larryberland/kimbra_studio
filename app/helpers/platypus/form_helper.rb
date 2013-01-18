c303ccd6339ab508d58bce7a3be692af0669c818
jimjames99 <jim@jimjames.org>
2013-01-17 21:26:15 -0500

    quick fix for platypus form helper.

--- app/helpers/platypus/form_helper.rb
+++ app/helpers/platypus/form_helper.rb
@@ -7,9 +7,9 @@ module Platypus
   module FormHelper
 
     def labeled_input(object_name, method, options = {})
-      input = text_field(object_name, method, options)
-      label = label(object_name, method, options)
-      #content_tag(:div, (label+input), { :class => 'labeled_input' }
+      input = content_tag(:div, text_field(object_name, method, options), {class: 'controls'})
+      label = label(object_name, method, options.merge({ class: 'control-label' }))
+      content_tag(:div, (label+input), { class: 'control-group' })
     end
 
   end
@@ -17,4 +17,3 @@ end
 
 ActionView::Helpers::FormBuilder.send(:include, Platypus::FormBuilder)
 ActionView::Helpers::FormHelper.send(:include, Platypus::FormHelper)
-
