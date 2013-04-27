cf4bcb723f256ede617777ad6d14e9ce13f6623e
larry berland <larryberland@gmail.com>
2013-04-27 15:16:59 -0400

    bring modal further down the screen

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module KimbraStudio
     end
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version                  = '2.1'
+    config.assets.version                  = '2.2'
 
     # Rspec generator functionality
     # :fixtures => true specifies to generate a fixture for each model
